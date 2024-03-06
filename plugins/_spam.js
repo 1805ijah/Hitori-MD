024eef992c493d2486fb0dbc5d021bbd934a47c6
1805ijah <161213262+1805ijah@users.noreply.github.com>
2024-03-06 11:43:30 +0800

    Delete plugins/_spam.js

--- plugins/_spam.js
+++
@@ -1,9 +0,0 @@
-import fetch from 'node-fetch'
-
-let handler = async (m, { conn }) => {
-	conn.sendFile(m.chat, './media/virgam.jpeg',20)
-}
-handler.command = /^(tes)$/i
-handler.tags = ['nocategory']
-handler.help = ['tes']
-export default handler
