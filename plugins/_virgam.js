8457aafa2feff47169339ab353f8a0f080d2367d
1805ijah <161213262+1805ijah@users.noreply.github.com>
2024-03-06 11:46:54 +0800

    Delete plugins/_virgam.js

--- plugins/_virgam.js
+++
@@ -1,9 +0,0 @@
-import fetch from 'node-fetch'
-
-let handler = async (m, { conn }) => {
-	conn.sendFile(m.chat, './media/virgam.jpeg')
-}
-handler.command = /^(virgam)$/i
-handler.tags = ['virus']
-handler.help = ['virgam']
-export default handler
